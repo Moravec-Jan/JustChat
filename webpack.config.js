6838fa97a95f1b80795ecc1e9e4f7060c304fcb9
JanMoravec <hmoravecb@seznam.cz>
2018-11-01 15:53:08 +0100

    basic project skeleton created with npm, webpack, babel.

---
+++ webpack.config.js
@@ -0,0 +1,15 @@
+const path = require("path");
+module.exports = {
+
+    entry: './scripts/app.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js',
+        publicPath: "/dist"
+    },
+    module: {
+        rules: [
+            {test: /\.js$/, exclude: /node_modules/, loader: "babel-loader"}
+        ]
+    }
+};
